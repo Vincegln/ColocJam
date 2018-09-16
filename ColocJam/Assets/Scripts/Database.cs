38b046ce678d3dca9f8f09b2f7230094031c5037
Unknown <thomas.sassano@insa-lyon.fr>
2018-09-16 14:42:24 -0400

    Database in place

---
+++ ColocJam/Assets/Scripts/Database.cs
@@ -0,0 +1,53 @@
+﻿public static class Database
+{
+	private static int combinaisonLength, score, lives;
+	private static float levelTime;
+
+	public static int CombinaisonLength 
+	{
+		get 
+		{
+			return combinaisonLength;
+		}
+		set 
+		{
+			combinaisonLength = value;
+		}
+	}
+	
+	public static int Score 
+	{
+		get 
+		{
+			return score;
+		}
+		set 
+		{
+			score = value;
+		}
+	}
+	
+	public static int Lives 
+	{
+		get 
+		{
+			return lives;
+		}
+		set 
+		{
+			lives = value;
+		}
+	}
+
+	public static float LevelTime 
+	{
+		get 
+		{
+			return levelTime;
+		}
+		set 
+		{
+			levelTime = value;
+		}
+	}
+}
