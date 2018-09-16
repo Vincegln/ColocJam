9e05d7c6c007cd45205042340c51bbc2d99a94c4
Vincent <vincent.guillon@hotmail.fr>
2018-09-16 14:21:24 -0400

    Preparation for singleton removal

--- ColocJam/Assets/Scripts/GameOver.cs
+++ ColocJam/Assets/Scripts/GameOver.cs
@@ -1,14 +1,43 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
     public Text Score;
     public Text Highscore;
+    
+    public Button RetryButton;
+    public Button MenuButton;
 
     private void Start()
     {
+        RetryButton.onClick.AddListener(RetryHandleClick);
+        MenuButton.onClick.AddListener(MenuHandleClick);
         Score.text = "Score : " + GameManager.Instance.Score;
-        Highscore.text = "Highscore :\r\n" + GameManager.Instance.HighScore;
+        Highscore.text = "Highscore :\r\n" + ReadString();
+    }
+    
+    public void RetryHandleClick()
+    {
+        GameManager.Instance.Score = 0;
+        GameManager.Instance.ChangeLevel();
+    }
+
+    public void MenuHandleClick()
+    {
+        Application.Quit();
+    }
+    
+    public string ReadString()
+    {
+        const string path = "Assets/Resources/highscore.txt";
+
+        //Read the text from directly from the test.txt file
+        var reader = new StreamReader(path);
+        var highscore = reader.ReadToEnd();
+        Debug.Log(highscore);
+        reader.Close();
+        return highscore;
     }
 }
