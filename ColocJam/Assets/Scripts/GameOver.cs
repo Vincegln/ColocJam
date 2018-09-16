de08ac2f8db0721f7ba872415c724e15519c722e
Vincent <vincent.guillon@hotmail.fr>
2018-09-16 10:49:58 -0400

    Code cleanup

--- ColocJam/Assets/Scripts/GameOver.cs
+++ ColocJam/Assets/Scripts/GameOver.cs
@@ -1,17 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Net.Mime;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
-	public Text Score;
-	public Text Highscore;
-	
-	private void Start ()
-	{
-		Score.text = "Score : " + GameManager.Instance.Score;
-		Highscore.text = "Highscore :\r\n" + GameManager.Instance.HighScore;
-	}
+    public Text Score;
+    public Text Highscore;
+
+    private void Start()
+    {
+        Score.text = "Score : " + GameManager.Instance.Score;
+        Highscore.text = "Highscore :\r\n" + GameManager.Instance.HighScore;
+    }
 }
--- ColocJam/Assets/Scripts/Menu.cs
+++ ColocJam/Assets/Scripts/Menu.cs
@@ -1,31 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class Menu : MonoBehaviour {
-    
-	public Button PlayButton;
-	public Button ExitButton;
-	public Text Highscore;
-    
-	// Use this for initialization
-	private void Start () 
-	{
-		PlayButton.onClick.AddListener (PlayHandleClick);
-		ExitButton.onClick.AddListener (ExitHandleClick);
-		GameManager.Instance.CurrentLevel = 999;
-		Highscore.text = "Highscore :\r\n" + GameManager.Instance.ReadString();
-	}
+public class Menu : MonoBehaviour
+{
+    public Button PlayButton;
+    public Button ExitButton;
+    public Text Highscore;
 
-	public void PlayHandleClick()
-	{
-		GameManager.Instance.ChangeLevel();
-	}
-	
-	public void ExitHandleClick()
-	{
-		Application.Quit();
-	}
+    // Use this for initialization
+    private void Start()
+    {
+        PlayButton.onClick.AddListener(PlayHandleClick);
+        ExitButton.onClick.AddListener(ExitHandleClick);
+        GameManager.Instance.CurrentLevel = 999;
+        Highscore.text = "Highscore :\r\n" + GameManager.Instance.ReadString();
+    }
+
+    public void PlayHandleClick()
+    {
+        GameManager.Instance.ChangeLevel();
+    }
+
+    public void ExitHandleClick()
+    {
+        Application.Quit();
+    }
 }
